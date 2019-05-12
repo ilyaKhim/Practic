cceadee01dd23d4e219f5844385c940d95ea594b
ilyaKhim <ilya.himchenko@yandex.ru>
2019-05-12 19:51:24 +0600

    init arr

--- src/Task1.java
+++ src/Task1.java
@@ -1,2 +1,39 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task1 {
+
+    public static int[] makeArr(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину массива: ");
+        int len = scanner.nextInt();
+        int[] arr = new int[len];
+        scanner.close();
+      return arr;
+    }
+
+    static void makeContent(int[] arr){
+        Random random = new Random();
+        for(int i = 0; i<arr.length;i++){
+            arr[i] = random.nextInt(arr.length);
+        }
+    }
+    static int[] doMerge(int[] arr){
+        int[] answer = new int[arr.length];
+
+        return answer;
+    }
+
+
+
+    public static void main(String[] args) {
+        int [] arr = makeArr();
+        makeContent(arr);
+        System.out.println(Arrays.toString(arr));
+        int[] answer = doMerge(arr);
+        System.out.println(Arrays.toString(answer));
+
+    }
 }
+
